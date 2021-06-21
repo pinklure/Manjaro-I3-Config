71e875034d3a7399e5b82bc494cbb25d9b62af59
SinowX <zhaoqifan0106@qq.com>
2021-06-21 11:02:43 +0800

    update

--- .screenlayout/DOUBLE.sh
+++ .screenlayout/DOUBLE.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 2160x1440 --pos 400x1440 --rotate normal --output DP1 --off --output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 2160x1440 --pos 2560x0 --rotate normal --output DP1 --off --output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
